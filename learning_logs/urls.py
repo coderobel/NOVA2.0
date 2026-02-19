d31dc68dd4ba4e9a67a0867c492bce8615f18cdf
coderobel <robel.ugr-4552-16@aau.edu.et>
2026-02-19 19:43:12 +0300

    made updates to template files

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,7 +4,6 @@ app_name='learning_logs'
 
 urlpatterns = [
     path('home/',views.home,name='home'),
-    path('topics/', views.topics, name='topics'),
     path('topic/<int:topic_id>', views.topic, name='topic'),
     path('newtopic/', views.newtopic, name='newtopic'),
     path('deletetopic/<int:topic_id>', views.deletetopic, name='deletetopic'),
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -5,11 +5,9 @@ from django.urls import reverse
 from .forms import TopicForm, EntryForm
 # Create your views here.
 def home(request):
-    return render(request, 'learning_logs/home.html')
-def topics(request):
     topics = Topic.objects.all()
     context = {'topics' : topics}
-    return render(request, 'learning_logs/topics.html', context)
+    return render(request, 'learning_logs/home.html', context)
 def topic(request, topic_id):
     topic = Topic.objects.get(id = topic_id)
     entries = topic.entry_set.order_by('-date_assigned')
@@ -22,14 +20,14 @@ def newtopic(request):
         form = TopicForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('learning_logs:topics'))
+            return HttpResponseRedirect(reverse('learning_logs:home'))
     context = {'form' : form}
     return render(request, 'learning_logs/newtopic.html', context)
 def deletetopic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     topic.delete()
-    return HttpResponseRedirect(reverse('learning_logs:topics'))
+    return HttpResponseRedirect(reverse('learning_logs:home'))
 def deleteentry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     entry.delete()
-    return HttpResponseRedirect(reverse('learning_logs:topics'))
+    return HttpResponseRedirect(reverse('learning_logs:home'))
